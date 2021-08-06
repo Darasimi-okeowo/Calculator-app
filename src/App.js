23b301f592341d9a71531b42bc247db94063c68c
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-08-06 12:20:44 +0100

    second-commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,49 @@ import './App.css';
 function App() {
   const [result,setResult] = useState('')
 
-  const handleChange = (e) => {
-    setResult(e.target.value)
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name))
   }
-  console.log('res',result)
+  const clear = () => {
+    setResult('')
+  }
+  const backSpace = () => {
+    setResult(result.slice(0,-1)) // result.length - 1 or -1
+  }
+  const calculate = () => {
+    try{
+      setResult(eval(result).toString());
+    }catch(err){
+      setResult("Error")
+    }
+  }
   return (
     <>
       <div className='container'>
         <form>
-          <input onChange={handleChange} value={result} type='text' />
+          <input  value={result} type='text' />
         </form>
+
+        <div className='keypad'>
+          <button onClick={clear}>Clear</button>
+          <button onClick={backSpace}>C</button>
+          <button name='/' onClick={handleClick}>÷</button>
+          <button name='7' onClick={handleClick}>7</button>
+          <button name='8' onClick={handleClick}>8</button>
+          <button name='9' onClick={handleClick}>9</button>
+          <button name='*' onClick={handleClick}>x</button>
+          <button name='4' onClick={handleClick}>4</button>
+          <button name='5' onClick={handleClick}>5</button>
+          <button name='6' onClick={handleClick}>6</button>
+          <button name='-' onClick={handleClick}>-</button>
+          <button name='1' onClick={handleClick}>1</button>
+          <button name='2' onClick={handleClick}>2</button>
+          <button name='3' onClick={handleClick}>3</button>
+          <button name='+' onClick={handleClick}>+</button>
+          <button name='0' onClick={handleClick}>0</button>
+          <button name='.' onClick={handleClick}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
       </div>
     </>
   );
