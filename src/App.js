cf01cd77b39037faeef9132e3157d9b5476e8296
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-08-06 11:30:12 +0100

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [result,setResult] = useState('')
+
+  const handleChange = (e) => {
+    setResult(e.target.value)
+  }
+  console.log('res',result)
   return (
-    <div className="App">
-      Calculator app
-    </div>
+    <>
+      <div className='container'>
+        <form>
+          <input onChange={handleChange} value={result} type='text' />
+        </form>
+      </div>
+    </>
   );
 }
 
